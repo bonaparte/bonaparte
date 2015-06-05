2a98bf7501443474da4b2f1114728f2ab9b0116d
Philipp Adrian <philipp.adrian@dowjones.com>
2015-06-05 17:37:11 -0400

    restucture 2.0 + readme

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,15 @@
 var gulp         = require('gulp');
+var spawn = require('child_process').spawn;
+
 // var watch         = require('gulp-watch');
 
 ///////////////////////////////////////////////////////////////////////////////
 
 gulp.task("default", ["watch"]);
 gulp.task("install", install);
-gulp.task("watch", ["save"], watch);
-gulp.task("save", save);
+gulp.task("watch", ["extract"], watch);
+gulp.task("watchBP", watchBP);
+gulp.task("extract", extract);
 // gulp.task("build", ["copy"], build);
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -14,32 +17,58 @@ gulp.task("save", save);
 
 function install(){
 
-  gulp.src('./bonaparte/**/*')
+  gulp.src('./dist/bonaparte/**/*')
     .pipe(gulp.dest('./SUI/src/themes/bonaparte')); 
    
-  gulp.src('./examples/**/*')
+  gulp.src('./dist/examples/**/*')
     .pipe(gulp.dest('./SUI/examples')); 
 
-  gulp.src('./theme.config')
+  gulp.src('./dist/theme.config')
     .pipe(gulp.dest('./SUI/src')); 
 
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 
-function save(){
+function extract(){
   gulp.src("./SUI/src/themes/bonaparte/**/*")
-    .pipe(gulp.dest('./bonaparte')); 
+    .pipe(gulp.dest('./dist/bonaparte')); 
    
   gulp.src('./SUI/examples/**/*')
-    .pipe(gulp.dest('./examples')); 
+    .pipe(gulp.dest('./dist/examples')); 
 
   gulp.src('./SUI/src/theme.config')
-    .pipe(gulp.dest('./')); 
+    .pipe(gulp.dest('./dist/')); 
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 
 function watch(){
-  gulp.watch(["./SUI/src/themes/bonaparte/**/*",'./SUI/examples/**/*', './SUI/src/theme.config'], ["save"]);
+
+  // spawn watcher
+  spawn('gulp', ["watchBP"]).stdout.on('data', function(data) {
+    if (data) {
+        console.log(data.toString())
+    }
+  });
+  
+
+  // cd to SUI and spawn watcher
+  process.chdir("./SUI/");
+
+  spawn('gulp', ["watch"]).stdout.on('data', function(data) {
+    if (data) {
+        console.log(data.toString())
+    }
+  });
+
 };
+
+///////////////////////////////////////////////////////////////////////////////
+
+
+function watchBP(){
+
+  gulp.watch(["./SUI/src/themes/bonaparte/**/*",'./SUI/examples/**/*', './SUI/src/theme.config'], ["extract"]);
+
+}
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Bonaparte Theme
+
+
+__DON`T WORK IN `dist/` DIRECTLY.__
+
+
+- Work in `SUI/`
+
+- Use `gulp watch` to watch SUI and keep `dist/` uptodate.
+
+- Use `gulp install` to copy `dist/` to `SUI/` (after reinstalling/updating SUI)
