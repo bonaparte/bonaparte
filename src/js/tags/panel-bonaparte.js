8eeab39dc30f8cda2a308124135ac190bc7d24f2
Philipp Adrian <philipp.adrian@dowjones.com>
2015-07-06 18:01:59 -0400

    define variable overrides.

--- src/js/tags/panel-bonaparte.js
+++ src/js/tags/panel-bonaparte.js
@@ -37,7 +37,7 @@ function panel(){
 ///////////////////////////////////////////////////////////////////////////////
 
   function attributeChangedCallback(data){
-    if(util.isAttribute("open", data.name)) {
+    if(util.isAttribute("open", data.name)){
       if(data.newValue == "true") {
         lock();
         tag.global.trigger("bonaparte:closePanels");
