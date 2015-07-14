32d34b882b6c5a4bb5e211cb48443fa727242d68
Philipp Adrian <philipp.adrian@dowjones.com>
2015-07-14 10:38:07 -0400

    remove size attribute form sidebar

--- src/js/tags/sidebar-bonaparte.js
+++ src/js/tags/sidebar-bonaparte.js
@@ -8,37 +8,7 @@ module.exports = registerTag("sidebar", sidebar);
 
 ///////////////////////////////////////////////////////////////////////////////
 function sidebar(tag){
-  var sidebar = tag.firstElementChild;
-  // updateSize();
 
-
-
-///////////////////////////////////////////////////////////////////////////////
-
-  tag.addEventListener("bonaparte.tag.attributeChanged", attributeChangedCallback);
-  
-///////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////
-
-  function attributeChangedCallback(data){
-    console.log("attribute changed");
-    // if(util.matchAttribute(/open/, data.detail.name) && data.detail.newValue !== "true") updateSize(data);
-  }
-
-
-///////////////////////////////////////////////////////////////////////////////
-
-  function updateSize(data){
-    console.log("update", data.detail);
-
-    var sidebar = util.getAttribute(tag, "sidebar");
-
-    if(sidebar === "left" || sidebar==="right")
-      tag.firstElementChild.style.width = tag.firstElementChild.offsetWidth+"px";
-    else 
-      tag.firstElementChild.style.height = tag.firstElementChild.offsetHeight+"px";
-
-  }
 
 }
 
