c183c9a8bba73520fa16de9bc54f0762ded8ad2d
Philipp Adrian <philipp.adrian@dowjones.com>
2015-07-16 11:10:59 -0400

    initialize buttons in toolbar -> make buttons initializable after window.load

--- src/js/tags/toolbar-bonaparte.js
+++ src/js/tags/toolbar-bonaparte.js
@@ -9,9 +9,32 @@ module.exports = registerTag("toolbar", toolbar, [
 ]);
 
 ///////////////////////////////////////////////////////////////////////////////
-function toolbar(){
+function toolbar(tag){
 
 
+  window.addEventListener("load", initializeButtons)
+
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+  
+  function initializeButtons(){
+    var groups = tag.firstElementChild.children;
+    var buttons=[];
+    for(var i=0; i<  groups.length; i++) {
+      for(var k=0; k<groups[i].children.length; k++) {
+
+        if(groups[i].children[k].nodeName.toUpperCase() !== "BUTTON-BONAPARTE")
+          buttons.push(groups[i].children[k]);
+      }
+
+    }
+
+    for(var i = 0; i< buttons.length; i++){
+      require("./button-bonaparte").initialize(buttons[i]);
+    }
+
+
+  }
 ///////////////////////////////////////////////////////////////////////////////
 
 }
