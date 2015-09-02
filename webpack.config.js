08213930fbb5b934ae0b2b383f95e07286547683
Philipp Adrian <philipp.adrian@dowjones.com>
2015-09-02 15:22:56 -0400

    simplify webpack.config, introduce "mandatory" bonaparte.build.js file

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,15 @@ module.exports = function(name){
     name = name || require("./package.json").name;
     var ExtractTextPlugin = require("extract-text-webpack-plugin");
     var ExtractCSS = new ExtractTextPlugin(name+".css");
-    var ExtractLESS = new ExtractTextPlugin(name+".less");
 
-    var entry = [
-        'var tag = require("./bonaparte.js");',
-        '((typeof tag === "object" || typeof tag === "function") && typeof tag.register === "function") && tag.register();',
-        'require("./bonaparte.less");'
-    ].join("\n");
+    // var entry = [
+    //     'var tag = require("./bonaparte.js");',
+    //     '((typeof tag === "object" || typeof tag === "function") && typeof tag.register === "function") && tag.register();',
+    //     'require("./bonaparte.less");'
+    // ].join("\n");
 
     return {
-        entry: [
-            'optional?./bonaparte.build.js!parse?'+entry+'!'     
-        ],
+        entry: './bonaparte.build.js!',
         output: {
             path: "./dist",
             filename: name+".js"
@@ -33,26 +30,10 @@ module.exports = function(name){
                 },
                 // Optionally extract less files
                 // or any other compile-to-css language
-                // {
-                //     test: /\.less$/,
-                //     loader: "raw-loader"
-                // }           
-                // Optionally extract less files
-                // or any other compile-to-css language
                 {
                     test: /\.less$/,
                     loader: ExtractCSS.extract("css-loader!autoprefixer-loader!less-loader")
                 }
-                // {
-                //     test: /\.less$/,
-                //     loader: "css-loader!autoprefixer-loader!less-loader"
-                // }            
-                // Optionally extract less files
-                // or any other compile-to-css language
-                // {
-                //     test: /\.less$/,
-                //     loader: ExtractLESS.extract("raw-loader")
-                // }
                 // You could also use other loaders the same way. I. e. the autoprefixer-loader
             ]
         },
