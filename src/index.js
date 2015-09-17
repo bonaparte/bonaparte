c5974cfe767f75fbaef3bc75c7168bb7ef59f51b
Philipp Adrian <philipp.adrian@dowjones.com>
2015-09-17 11:50:57 -0400

    fix index.js?

--- src/index.js
+++ src/index.js
@@ -6,17 +6,19 @@ module.exports = require("./api");
 ///////////////////////////////////////////////////////////////////////////////
 // Polyfills
 
-if(typeof document.addEventListener === "function") return; // no polyfills for IE8 -> silently fail.
+if(typeof document.addEventListener === "function") { // no polyfills for IE8 -> silently fail.
+  
+  if(!("MutationObserver" in document)) {
+    MutationObserver = require("mutation-observer");
+  };
+  require("document-register-element");
+  require("custom-event-polyfill");
 
-if(!("MutationObserver" in document)) {
-  MutationObserver = require("mutation-observer");
-};
-require("document-register-element");
-require("custom-event-polyfill");
 
-if (Element && !Element.prototype.matches) {
-    var proto = Element.prototype;
-    proto.matches = proto.matchesSelector ||
-        proto.mozMatchesSelector || proto.msMatchesSelector ||
-        proto.oMatchesSelector || proto.webkitMatchesSelector;
+  if (Element && !Element.prototype.matches) {
+      var proto = Element.prototype;
+      proto.matches = proto.matchesSelector ||
+          proto.mozMatchesSelector || proto.msMatchesSelector ||
+          proto.oMatchesSelector || proto.webkitMatchesSelector;
+  }
 }
