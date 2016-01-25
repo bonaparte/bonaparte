e1c9da39f6f4ffe0a8d66f1b623dd13870fb931e
Philipp Adrian <philipp.adrian@dowjones.com>
2016-01-25 16:35:10 -0500

    update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,37 @@
-module.exports = function(name){
-    name = name || require("./package.json").name;
-    var ExtractTextPlugin = require("extract-text-webpack-plugin");
-    var ExtractCSS = new ExtractTextPlugin(name+".css");
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var ExtractCSS = new ExtractTextPlugin("bonaparte.css");
 
-    return {
-        entry: './bonaparte.build.js',
-        output: {
-            path: "./dist",
-            filename: name+".js"
-        },
-        module: {
-            loaders: [
-                {
-                    test: /\.css$/i,
-                    loader: ExtractCSS.extract("css-loader!autoprefixer-loader")
-                },
-                {
-                    test: /\.less$/i,
-                    loader: ExtractCSS.extract("css-loader!autoprefixer-loader!less-loader")
-                },
-                {
-                    test: /\.scss$/i,
-                    loader: ExtractCSS.extract("css-loader!autoprefixer-loader!sass-loader")
-                },
-                {
-                    test: /\.(woff|woff2|eot|ttf)$/i,
-                    loader: "file-loader?name=fonts/[name]-[hash].[ext]"
-                },
-                {
-                    test: /\.(jpe?g|png|gif|svg)$/i,
-                    loader: "file-loader?name=images/[name]-[hash].[ext]"
-                }
-            ]
-        },
-        plugins: [
-           ExtractCSS,
+module.exports =  {
+    entry: './bonaparte.build.js',
+    output: {
+        path: "./dist",
+        filename: "bonaparte.js"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.css$/i,
+                loader: ExtractCSS.extract("css-loader!autoprefixer-loader")
+            },
+            {
+                test: /\.less$/i,
+                loader: ExtractCSS.extract("css-loader!autoprefixer-loader!less-loader")
+            },
+            {
+                test: /\.scss$/i,
+                loader: ExtractCSS.extract("css-loader!autoprefixer-loader!sass-loader")
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf)$/i,
+                loader: "file-loader?name=fonts/[name]-[hash].[ext]"
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                loader: "file-loader?name=images/[name]-[hash].[ext]"
+            }
         ]
-    }
-}
+    },
+    plugins: [
+       ExtractCSS,
+    ]
+};
