31929082cf1e1c76b2127e98d219d938cc79d061
Philipp Adrian <hello@philippadrian.com>
2016-02-07 13:56:17 -0500

    Update js/github-link.js

--- js/github-link.js
+++ js/github-link.js
@@ -1,8 +1,8 @@
+document.querySelector("title").innerHTML = "Bonaparte Documentation";
+
 window.addEventListener("load", function(){
     var link = document.querySelector("a.custom-link");
     link.innerHTML = "Visit Bonaparte on Github";
     link.setAttribute('style', 'text-indent:0;');
     link.setAttribute('href', 'http://github.com/bonaparte');
 });
-
-document.querySelector("title").innerHTML = "Bonaparte Documentation";
