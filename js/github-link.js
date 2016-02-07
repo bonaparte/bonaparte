4a5ff21fa34b0b30c28c968324e470343e0afe1f
Philipp Adrian <hello@philippadrian.com>
2016-02-07 13:48:51 -0500

    Update js/github-link.js

--- js/github-link.js
+++ js/github-link.js
@@ -2,8 +2,16 @@ window.addEventListener("load", function(){
     var summary = document.querySelector("ul.summary");
     
     var firstElement = summary.firstChild;
-    
+
+    var li = document.createElement('li');
+    var link = document.createElement('a');
+    link.innerHTML = "Bonaparte on Github";
+    link.setAttribute('class', 'my-custom-link');
+    link.setAttribute('href', 'http://github.com/bonaparte');
+    link.setAttribute('target', '_blank');
     console.log(firstElement);
+    
+    
     
 });
 
