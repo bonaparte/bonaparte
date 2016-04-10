04c27605e9a0d5a8c232e718266a6e7e8045d1a3
Philipp Adrian <philipp.adrian@dowjones.com>
2016-04-09 20:37:32 -0400

    remove package/bonaparte-build

--- packages/bonaparte-build/bonaparte.build.js
+++
@@ -1,9 +0,0 @@
-// Components
-require("bonaparte-panel");
-require("bonaparte-scroll");
-require("bonaparte-button");
-require("bonaparte-sidebar");
-require("bonaparte-toolbar");
-require("bonaparte-draggable");
-require("bonaparte-dropdown");
-require("bonaparte-collapsible");
--- packages/bonaparte-build/webpack.config.js
+++
@@ -1,1 +0,0 @@
-module.exports = require('bonaparte/webpack.config');
